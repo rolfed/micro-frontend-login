db3a567054529b2b6a17b6e65ff6737b88a6c3e0
Danniel Rolfe <dannielrolfe@umpquabank.com>
2021-09-09 21:36:07 -0700

    WIP

--- projects/login/src/lib/login/login.component.spec.ts
+++ projects/login/src/lib/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,9 +9,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      declarations: [LoginComponent],
+      imports: [RouterTestingModule]
+    }).compileComponents();
   });
 
   beforeEach(() => {
--- projects/login/src/lib/login/login.component.ts
+++ projects/login/src/lib/login/login.component.ts
@@ -1,59 +1,69 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'umpua-bank-login',
   template: `
-  <main class="container p-5">
-
-    <div class="row">
-      <div class="col-6 offset-3">
-        <ucl-title>Login (UCL Source)</ucl-title>
+    <main class="container p-5">
+      <div class="row">
+        <div class="col-6 offset-3">
+          <h1>Login</h1>
+        </div>
       </div>
-    </div>
 
-    <div class="row">
-      <div class="col-6 offset-3">
-        <form [formGroup]="loginFG">
-          <div class="form-group mb-3">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email"
-              class="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Enter email"
-              [formControl]="emailFC">
-          </div>
-          <div class="form-group mb-3">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password"
-              class="form-control"
-              id="exampleInputPassword1"
-              placeholder="Password"
-              [formControl]="passwordFC">
-          </div>
+      <div class="row">
+        <div class="col-6 offset-3">
+          <form [formGroup]="loginFG">
+            <div class="form-group mb-3">
+              <label for="exampleInputEmail1">Email address</label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+                [formControl]="emailFC"
+              />
+            </div>
+            <div class="form-group mb-3">
+              <label for="exampleInputPassword1">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                placeholder="Password"
+                [formControl]="passwordFC"
+              />
+            </div>
 
-          <div class="form-group mb-5">
-            <button type="submit"
-              class="btn btn-primary"
-              (click)="onSubmit()">Submit</button>
-          </div>
-        </form>
+            <div class="form-group mb-5">
+              <button
+                type="submit"
+                class="btn btn-primary"
+                (click)="onSubmit()"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
-  </main>
-`, styles: [
-  ]
+    </main>
+  `,
+  styles: []
 })
 export class LoginComponent {
   public emailFC: FormControl = new FormControl('', []);
   public passwordFC: FormControl = new FormControl('', []);
   public loginFG: FormGroup = new FormGroup({
-      login: this.emailFC,
-      password: this.passwordFC
-    });
+    login: this.emailFC,
+    password: this.passwordFC
+  });
+
+  constructor(private router: Router) {}
 
   public onSubmit(): void {
-    console.log('FORM GROUP: ', this.loginFG.value);
+    this.router.navigate(['/about']);
   }
 }
